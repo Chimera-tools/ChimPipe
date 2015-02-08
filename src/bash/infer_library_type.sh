81f823c0c193cbd70bf557309732e96ce3459292
Bernardo Rdgz <rodriguezmartinbernardo@gmail.com>
2015-02-08 19:35:24 +0100

    INFER LIBRARY STEP. Generate random seed for samtools view -s

--- src/bash/infer_library_type.sh
+++ src/bash/infer_library_type.sh
@@ -91,8 +91,14 @@ cutgff=$awkDir/cutgff.awk
 # START
 ########
 ## Comment: samtools view -F 260 ** filter out unmapped reads (4) + secondary alignments (254) = 260
+## Note about random sampling with samtools view -s:
+# Current versions of samtools (ab)use the integer part of the -s value to set the seed:
+# -s FLOAT    Integer part is used to seed the random number generator. Part after the decimal point sets the fraction of templates/pairs to subsample
+# So two runs with e.g. view -s 123.1 / view -s 456.1 should select two distinct randomly-selected 10% subsets.
+# I will take a random number between 0 and 10 as a seed
+randomSeed=`shuf -i1-10 -n1`
 
-awk -v elt='exon' '$3==elt' $annot | awk -v to=8 -f $cutgff | sort -k1,1 -k4,4 -k5,5 | uniq | bedtools intersect -abam <(samtools view -b -s 1.001 -F 260 $bamfile) -b stdin -split -bed -wo | awk '{print $4, $6, $19;}' | uniq | awk '{split($1,a,"/"); readCount["total"]++; readCount[a[2]":"$2":"$3]++;}END{fraction1=(readCount["1:+:+"]+readCount["1:-:-"]+readCount["2:+:-"]+readCount["2:-:+"]); fraction2=(readCount["1:+:-"]+readCount["1:-:+"]+readCount["2:+:+"]+readCount["2:-:-"]); other=(readCount["total"]-(fraction1+fraction2)); print (fraction1/readCount["total"]*100), (fraction2/readCount["total"]*100), other;}' 
+awk -v elt='exon' '$3==elt' $annot | awk -v to=8 -f $cutgff | sort -k1,1 -k4,4 -k5,5 | uniq | bedtools intersect -abam <(samtools view -b -s ${randomSeed}.01 -F 260 $bamfile) -b stdin -split -bed -wo | awk '{print $4, $6, $19;}' | uniq | awk '{split($1,a,"/"); readCount["total"]++; readCount[a[2]":"$2":"$3]++;}END{fraction1=(readCount["1:+:+"]+readCount["1:-:-"]+readCount["2:+:-"]+readCount["2:-:+"]); fraction2=(readCount["1:+:-"]+readCount["1:-:+"]+readCount["2:+:+"]+readCount["2:-:-"]); other=(readCount["total"]-(fraction1+fraction2)); print (fraction1/readCount["total"]*100), (fraction2/readCount["total"]*100), other;}' 
 
 
 
